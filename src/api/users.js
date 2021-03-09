05d6f832e4ddfd68416bc413b4e9778569444edb
Abdug'affor Abdurahimov <abdurahimov.97@list.ru>
2021-03-09 07:48:31 +0500

    updated

--- src/api/users.js
+++ src/api/users.js
@@ -1,4 +1,4 @@
-const UserModel = require("../models/user");
+const UserModel = require("../models/userModel");
 const { getTokenPairs } = require("../utils");
 
 const router = require("express").Router();

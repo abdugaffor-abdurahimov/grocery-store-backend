76ad39652ea950e3d2c4e844dce572ef81943978
Abdug'affor Abdurahimov <abdurahimov.97@list.ru>
2021-04-03 23:06:47 +0500

    image uploading implemented to cloudinary

--- src/admin/components/Dashboard.jsx
+++ src/admin/components/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { ApiClient, useAction } from "admin-bro";
+import { ApiClient } from "admin-bro";
 import React, { useState, useEffect } from "react";
 
 import { Box } from "@admin-bro/design-system";
@@ -7,18 +7,6 @@ const api = new ApiClient();
 
 const Dashboard = () => {
   const [data, setData] = useState({});
-
-  const { href, handleClick } = useAction(
-    action,
-    {
-      resourceId,
-      recordId,
-      recordIds,
-    },
-    actionPerformed
-  );
-
-  console.log(action);
 
   useEffect(() => {
     api.getDashboard().then((response) => {
@@ -30,9 +18,8 @@ const Dashboard = () => {
     <Box variant="grey">
       <Box variant="white">
         <h1>Grocery store {data.some}</h1>
-        <Button as="a" onClick={handleClick} href={href}>
-          Click this action
-        </Button>
+        <br />
+        <a href="/api/api-docs">API Documentation</a>
       </Box>
     </Box>
   );

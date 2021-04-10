de8685f708b8091b4e80220770cfb2184da461d9
Abdug'affor Abdurahimov <abdurahimov.97@list.ru>
2021-04-10 07:10:34 +0500

    Sending products ingo to users email imp

---
+++ src/utils/emailAttachment/index.js
@@ -0,0 +1,41 @@
+const nodemailer = require("nodemailer");
+const htmlToPdfBuffer = require("./pdf");
+
+const senEmailWithAttachment = async (userAddress) => {
+  try {
+    const fileBuffer = await htmlToPdfBuffer(userAddress);
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        type: "OAuth2",
+        user: process.env.MAIL_USERNAME,
+        pass: process.env.MAIL_PASSWORD,
+        clientId: process.env.OAUTH_CLIENTID,
+        clientSecret: process.env.OAUTH_CLIENT_SECRET,
+        refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+      },
+    });
+
+    const mailOptions = {
+      from: "abdulgaffaribnrasul@gmail.com",
+      to: userAddress,
+      subject: "Billing Information",
+      text: "Hi " + userAddress,
+      html: fileBuffer,
+    };
+
+    transporter.sendMail(mailOptions, function (err, data) {
+      return new Promise((resolve, reject) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(data);
+        }
+      });
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = senEmailWithAttachment;
